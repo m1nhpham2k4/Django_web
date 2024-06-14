d2c29806f34b7a2c3c4d3fa4def92327e3795c74
m1nhpham2k4 <minh02122004@gmail.com>
2024-06-14 17:04:46 +0700

    Django_web

--- webbanhang/app/urls.py
+++ webbanhang/app/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
+    path('card/', views.card, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
