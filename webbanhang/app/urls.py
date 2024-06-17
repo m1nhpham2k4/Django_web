7a9d93ca857b87f4c38e20a0b724213fbd4d671f
m1nhpham2k4 <minh02122004@gmail.com>
2024-06-17 17:02:12 +0700

    Django_web

--- webbanhang/app/urls.py
+++ webbanhang/app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
-    path('card/', views.card, name="cart"),
+    path('card/', views.card, name="card"),
     path('checkout/', views.checkout, name="checkout"),
 ]
